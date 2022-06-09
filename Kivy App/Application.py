57e4543e205c7ebe4e01ecde00ed1ca5be122a24
Adeep Krishna Keelar <69258931+AdeepKrishnaKeelar@users.noreply.github.com>
2022-06-09 09:18:20 +0530

    Hate uploading files, breaks my heart

--- Kivy App/Application.py
+++ Kivy App/Application.py
@@ -5,15 +5,24 @@ Install Kivy by using pip3/pip install kivy
 """
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 class MainApp(App):
-    def build(self):
+    """def build(self):
         label = Label(
-            text='FOULGAS APPLICATION',
+            text='FOULGAS APPLICATION BY APEED',
             size_hint=(.5,.5),
             pos_hint={'center_x':.5,'center_y':.5})
         
         return label
+    """
+    def build(self):
+        img = Image(
+            source='foulgas.png',
+            size_hint=(1,.5),
+            pos_hint={'center_x':.5,'center_y':.5})
+        
+        return img
 
 if __name__ == '__main__':
     app = MainApp()
