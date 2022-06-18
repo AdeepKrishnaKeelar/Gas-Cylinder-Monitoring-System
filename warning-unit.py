ed7526b7f210c69da664b842ed631328d59d3cbd
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-06-18 13:16:42 +0530

    Server program

---
+++ warning-unit.py
@@ -0,0 +1,11 @@
+"""
+The Warning module of the Server 
+"""
+from easygui import *
+from playsound import playsound
+message = "WARNING"
+title = "WARNING"
+ok_button = "CONTINUE"
+playsound('C:\Users\eippe\OneDrive\Desktop\Minor Project\Warning-sound.mp3')
+output=msgbox(message)
+print(output)
